8bf196b9b57367c57a6cad3f65500528c57c6223
nana1410 <37500796+nana1410@users.noreply.github.com>
2018-04-05 23:50:14 +0900

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,20 +2,12 @@
 
 export const environment = {
   firebase: {
-    apiKey: firebase.apiKey,
-    authDomain: firebase.authDomain,
-    databaseURL: firebase.databaseURL,
-    projectId: firebase.projectId,
-    storageBucket: firebase.storageBucket,
-    messagingSenderId: firebase.messagingSenderId
+    apiKey: "AIzaSyCE2meL9-CIBqBt1mDGRQX2oQXFDWYSxTw",
+    authDomain: "chatonmap-e8be1.firebaseapp.com",
+    databaseURL: "https://chatonmap-e8be1.firebaseio.com",
+    projectId: "chatonmap-e8be1",
+    storageBucket: "chatonmap-e8be1.appspot.com",
+    messagingSenderId: "172350505295"
   }
 };
 
-export const firebaseKey = {
-  'apiKey': 'AIzaSyCE2meL9-CIBqBt1mDGRQX2oQXFDWYSxTw',
-  'authDomain': 'chatonmap-e8be1.firebaseapp.com',
-  'databaseURL': 'https://chatonmap-e8be1.firebaseio.com',
-  'projectId': 'chatonmap-e8be1',
-  'storageBucket': 'chatonmap-e8be1.appspot.com',
-  'messagingSenderId': '172350505295'
-};

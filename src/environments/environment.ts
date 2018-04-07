fec1b5deb65621e7a2edc696585f1817235717bf
nana1410 <37500796+nana1410@users.noreply.github.com>
2018-04-07 10:03:19 +0900

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
-//import { firebase } from '../../config/firebase';
+import { firebase } from '../../config/firebase';
 
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyCE2meL9-CIBqBt1mDGRQX2oQXFDWYSxTw",
-    authDomain: "chatonmap-e8be1.firebaseapp.com",
-    databaseURL: "https://chatonmap-e8be1.firebaseio.com",
-    projectId: "chatonmap-e8be1",
-    storageBucket: "chatonmap-e8be1.appspot.com",
-    messagingSenderId: "172350505295"
+    apiKey: firebase.apiKey,
+    authDomain: firebase.authDomain,
+    databaseURL: firebase.databaseURL,
+    projectId: firebase.projectId,
+    storageBucket: firebase.storageBucket,
+    messagingSenderId: firebase.messagingSenderId
   }
 };
 
